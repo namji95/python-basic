9a88d3f3da43e2cf441873ac5fc92e38fe8423af
namji <skawlgns07@naver.com>
2024-07-01 10:30:28 +0900

    feat(dataframe): 데이터 프레임 예제
    데이터 프레임 데이터 추가 및 삭제

--- dataframe/data_frame_basic.py
+++ dataframe/data_frame_basic.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 class DataFrameBasic:
     def data_frame_basic(self):
-        print("판다스 데이터 프레임 활용 예제")
+        print("판다스 데이터 프레임 활용 예제\n")
 
-        print("\n데이터 프레임 생성")
+        print("--------------데이터 프레임 생성--------------")
         data = {
             '이름': ['홍길동', '김철수', '이영희'],
             '나이': [25, 30, 22],
@@ -13,21 +13,27 @@ class DataFrameBasic:
         }
         # 데이터 프레임 생성 방법
         df = pd.DataFrame(data)
-        print(df)
+        print(f"데이터 프레임 생성:\n{df}")
 
-        print("\n데이터 프레임 기본 정보 확인")
+        print("\n--------------데이터 프레임 기본 정보 확인--------------")
         # 데이터 프레임 정보 출력
-        print("df.info(): ", df.info())
+        print(f"df.info():\n{df.info()}")
         # 데이터 프레임의 통계 요약 출력
-        print("\ndf.describe(): \n", df.describe())
+        print(f"\ndf.describe():\n{df.describe()}")
 
-        print("\n데이터 선택 및 필터링")
+        print("\n--------------데이터 선택 및 필터링--------------")
         # 특정 열 선택
-        print(f"\n특정 열 선택:\n{df['이름']}")
+        print(f"특정 열 선택:\n{df['이름']}")
         # 여러 열 선택
         print(f"\n여러 열 선택:\n{df[['이름', '나이']]}")
         # 조건에 맞는 행 필터링
         print(f"\n조건에 맞는 행 필터링:\n{df[df['나이'] > 25]}")
+
+        print("\n--------------데이터 추가 및 삭제--------------")
+        df['국가'] = ['한국', '한국', '한국']
+        print(f"데이터 추가:\n{df}")
+        df = df.drop('국가', axis=1)
+        print(f"열 삭제:\n{df}")
 
 if __name__ == '__main__':
     dfb = DataFrameBasic()
