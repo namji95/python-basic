775e0d0a7cd7e7200e2356f3d770d5811b965b37
namji <skawlgns07@naver.com>
2024-07-26 08:46:32 +0900

    feat(selenium): 웹 크롤링 라이브러리 selenium 예제
    웹 크롤링 라이브러리 selenium 사용한 예제 코드
    크롬 웹 페이지 접속까지 기능 구현

---
+++ crawler/selenium_test.py
@@ -0,0 +1,25 @@
+# selenium의 webdriver를 사용하기 위한 import
+from selenium import webdriver
+
+# selenium으로 키를 조작하기 위한 import
+# from selenium.webdriver.common.keys import Keys
+
+# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
+import time
+
+class SeleniumTest:
+    def start_selenium(self):
+        # 크롬드라이버 실행
+        driver = webdriver.Chrome()
+
+        # 크롬 드라이버에 url 주소 넣고 실행
+        driver.get('https://www.google.co.kr/')
+
+        # 페이지가 완전히 로딩되도록 3초동안 기다림
+        time.sleep(3)
+
+if __name__ == '__main__':
+    selenium_test = SeleniumTest()
+    selenium_test.start_selenium()
+
+# todo: 파일 이름 또는 클래스 이름이 라이브러리 이름과 같을 경우 에러 발생 예약어 조심
