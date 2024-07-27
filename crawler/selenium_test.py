45f7a7a18acc9d70b921bcfc283578f02d123b0c
namji <skawlgns07@naver.com>
2024-07-28 04:15:08 +0900

    refactor(selenium): 웹 크롤링 라이브러리 selenium 예제
    웹 크롤링 라이브러리 selenium 사용한 예제 코드
    크롬 웹 페이지 접속 후 특정 엘리먼트 추출 후 해당 데이터 출력 기능 구현

--- crawler/selenium_test.py
+++ crawler/selenium_test.py
@@ -9,7 +9,7 @@ import time
 class SeleniumTest:
     def start_selenium(self):
         options = Options()
-        options.add_argument("")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
         options.add_argument("--disable-blink-features=AutomationControlled")  # 자동화 탐지 방지
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
@@ -23,15 +23,27 @@ class SeleniumTest:
 
         time.sleep(3)
 
-        win_numbers = driver.find_element(By.CLASS_NAME, "num.win")
+        # 부모 div 요소를 기준으로 내부 span 태그 모두 찾기
+        span_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'num win')]//span"))
+        )
 
-        print("win_numbers.text: ", win_numbers.text)
+        number_list = []
 
-        time.sleep(3)
+        for idx, element in enumerate(span_elements):
+            number_list.append({f"{idx+1}번 볼": element.text})
 
-        bonus_number = driver.find_element(By.CLASS_NAME, "num.bonus")
+        time.sleep(2)
 
-        print("bonus_number.text: ", bonus_number.text)
+        # 부모 div 요소를 기준으로 내부 span 태그 모두 찾기
+        span_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'num bonus')]//span"))
+        )
+        
+        for idx, element in enumerate(span_element):
+            number_list.append({"bonus 볼": element.text})
+
+        print(number_list)
 
         driver.quit()
 
