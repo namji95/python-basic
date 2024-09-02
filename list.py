9d6a653c1aa63b08300d5b6325a736ab88ef1ca8
namji95 <skawlgns07@naver.com>
2024-09-02 16:24:55 +0900

    파이썬 기본 사용법

---
+++ list.py
@@ -0,0 +1,6 @@
+family = ['mother', 'father', 'gentleman', 'sexy lady']
+print(len(family))
+print(family[3])
+print(family)
+family.remove('gentleman')
+print(family)
---
+++ valiable.py
@@ -0,0 +1,23 @@
+watch = 1000000
+lighter = 300
+
+print(watch)
+print(lighter)
+print(watch + lighter)
+
+print(watch * 0.85)
+
+price = 5000
+price = price * 0.85
+print(price)
+
+a = 'pig'
+print(a)
+b = 'dad'
+print(a + b)
+print(a + ' ' + b)
+
+file = 800
+time = 110
+total = file * time / 1000
+print(total)
