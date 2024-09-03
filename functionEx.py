5eda0195422a32870b534a31e75dc6a6d59e69bb
namji95 <skawlgns07@naver.com>
2024-09-03 14:45:42 +0900

    함수 작성법

---
+++ functionEx.py
@@ -0,0 +1,11 @@
+a_list = [3, 4, 62, 27, 83, 956, 26, 58, 3, 78, 168, 64, 78]
+
+def print_list(a):
+    for i in a:
+        print(i)
+
+print_list(a_list)
+
+def boy():
+    print('I am a boy')
+    print('You are a girl')
