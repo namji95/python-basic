65e86899d3229c55065ae65c523466ccc57f7341
namji95 <skawlgns07@naver.com>
2024-09-03 16:38:33 +0900

    feat: 한국 소설 운수 좋은 날에서 가장 많이 나온 단어 20개 추출 과제
    class형으로 수정
    로직에서 함수로 분리 가능한 부분 함수화
    init 함수 추가
    main 함수 추가
    수정

--- functionEx.py
+++ functionEx.py
@@ -6,6 +6,37 @@ def print_list(a):
 
 print_list(a_list)
 
-def boy():
-    print('I am a boy')
-    print('You are a girl')
+def scope_test():
+    def do_local():
+        spam = "local spam"
+    
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+    
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+print('\n함수 연습 문제 1')
+def ipCount(x, y):
+    result = x ** y
+    return result
+
+print(ipCount(int(input()), int(input())))
+
+def numOfDigits(num):
+    print(len(str(num )))
+
+numOfDigits(int(input()))
