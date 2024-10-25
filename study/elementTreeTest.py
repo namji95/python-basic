6972444e053f2e4bbb2909f412a0d710e24140cc
namji95 <skawlgns07@naver.com>
2024-10-25 16:22:48 +0900

    ElementTree 사용법 예제 코드

---
+++ study/elementTreeTest.py
@@ -0,0 +1,35 @@
+import xml.etree.cElementTree as ET
+from lib2to3.pgen2.tokenize import printtoken
+
+xml = """<?xml version = "1.0" encoding = "UTF-8" ?> 
+<root> 
+    <sub rank="1" id="apple" price="120" u="up">애플
+        <desc name="ipod">아이팟</desc>
+        <desc name="phone">아이폰</desc>       
+    </sub>
+    <sub rank="2" id="tsla" price="820" u="up">테슬라</sub>
+    <sub rank="3" id="msft" price="233" u="down">마소</sub>
+</root>
+"""
+
+r = ET.fromstring(xml)
+print("=========root 하위 find사용 직접 꺼내기=========")
+print(r.find('sub').get('rank'))
+print(r.find('sub')[0].get('name'))
+print(r.find('sub')[1].text)
+print(r[0].text)
+
+print("=========root 하위 get 직접꺼내기=========")
+print(r[1].get("rank"))
+print(r[1].get("id"))
+print(r[1].get("price"))
+print(r[1].text)
+
+print("\n=========root 하위 get 반복문=========")
+for tag in r:
+    print(tag.get("rank"))
+    print(tag.get("id"))
+    print(tag.get("price"))
+    print(tag.text)
+    if len(tag) > 0:
+        print(",".join(v.get("name") for v in tag))
