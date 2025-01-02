8f87af8824f79c6d8287c3ce3134cc63a8871ee0
namji <namjihun1@gamil.com>
2025-01-02 13:04:13 +0900

    feat(for): for문 활용 예제
    튜플을 활용한 for문 예제 3

--- for/for_ex.py
+++ for/for_ex.py
@@ -13,6 +13,11 @@ class ForEx:
         for char in word:
             print(f"문자열을 이용한 for문 : {char}")
 
+        print("\n튜플을 활용한 for문 예제 3")
+        coordinates = [(1, 2), (3, 4), (5, 6)]
+        for x, y in coordinates:
+            print(f"튜플을 이용한 for문 : {x}, ")
+
 if __name__ == '__main__':
     for_ex = ForEx()
     for_ex.iterable_list()
