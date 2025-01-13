3e4d2f0cc402fff46398fe7a4e869dc5bee11f8e
namji <skawlgns07@naver.com>
2025-01-13 12:38:29 +0900

    feat(enumerate): enumerate 예시 코드2 을 활용한 enumerate 예시 코드

--- for/enumerate_ex.py
+++ for/enumerate_ex.py
@@ -5,3 +5,8 @@ print("\n문자열을 이용한 enumerate 출력 예시")
 str_data = "enumerate example"
 for index, value in enumerate(str_data):
     print(index, "번: ", value)
+
+print("\n리스트를 이용한 enumerate 출력 예시")
+list_data = [1, 2, 3, 4, 5]
+for index, value in enumerate(list_data):
+    print(index, "번: ", value)
