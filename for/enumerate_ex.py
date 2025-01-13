cb60371824859146e98ff4d764835664b6de8d2b
namji <skawlgns07@naver.com>
2025-01-13 12:39:11 +0900

    feat(enumerate): enumerate 예시 코드2
    리스트를 활용한 enumerate 예시 코드2

--- for/enumerate_ex.py
+++ for/enumerate_ex.py
@@ -7,6 +7,6 @@ for index, value in enumerate(str_data):
     print(index, "번: ", value)
 
 print("\n리스트를 이용한 enumerate 출력 예시")
-list_data = [1, 2, 3, 4, 5]
+list_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for index, value in enumerate(list_data):
     print(index, "번: ", value)
