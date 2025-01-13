618be1a6b9a216563719b2fa595628c8ce270362
namji <skawlgns07@naver.com>
2025-01-13 12:42:21 +0900

    feat(enumerate): enumerate 예시 코드3
    딕셔너리를 활용한 enumerate 예시 코드3

--- for/enumerate_ex.py
+++ for/enumerate_ex.py
@@ -10,3 +10,14 @@ print("\n리스트를 이용한 enumerate 출력 예시")
 list_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for index, value in enumerate(list_data):
     print(index, "번: ", value)
+
+print("\n딕셔너리를 이용한 enumerate 출력 예시")
+dict_data = {
+    "1": "A",
+    "2": "B",
+    "3": "C",
+    "4": "D",
+    "5": "E",
+}
+for index, (key, value) in enumerate(dict_data.items()):
+    print(f"{index}번 {key}: {value}")
