2dbd1d3dd9fc7a191639d8e5ca17157d11624bf1
namji <skawlgns07@naver.com>
2025-01-15 12:50:48 +0900

    feat(isinstance): isinstance를 활용하여 타입 찾기
    설정 변수가 정수일 때 isinstance에 정수 체크하도록 설정하고 다른 isinstance에는 문자열을 체크하도록 하여 결과 확인하는 예제 코드

--- type/isinstance_ex.py
+++ type/isinstance_ex.py
@@ -3,3 +3,7 @@ print(f"isinstance 문자열 체크 예제")
 data_str = "문자열 체크 테스트"
 print(f"({data_str}) 데이터가 {type(data_str)}일 때 isinstance str로 설정 결과: {isinstance(data_str, str)}")
 print(f"({data_str}) 데이터가 {type(data_str)}일 때 isinstance int로 설정 결과: {isinstance(data_str, int)}")
+
+data_int = 123
+print(f"({data_int}) 데이터가 {type(data_int)}일 때 isinstance str로 설정 결과: {isinstance(data_int, int)}")
+print(f"({data_int}) 데이터가 {type(data_int)}일 때 isinstance int로 설정 결과: {isinstance(data_int, str)}")
