49046ed20a0d746ae54fb70ee72804059f8e1529
namji <skawlgns07@naver.com>
2025-01-19 17:09:45 +0900

    feat(tqdm): tqdm 라이브러리를 활용한 예시 코드 작성
    tqdm 라이브러리와 enumerate 활용 예시 코드 작성

--- progress/tqdm_test.py
+++ progress/tqdm_test.py
@@ -6,9 +6,15 @@ class TqdmTest:
     def tqdm_test(self):
         print("tqdm 라이브러리를 활용한 예시 코드")
 
-        print("-----tqdm 기초 사용법-----")
-        for i in tqdm(range(10), desc="Progressing"):
-            time.sleep(0.5)
+        print("\n-----tqdm 기초 사용법-----")
+        for i in tqdm(range(5), desc="Progressing"):
+            time.sleep(0.1)
+
+        print("\n-----tqdm 기초 사용법-----")
+        data = ["A", "B", "C", "D", "E"]
+        for idx, value in tqdm(enumerate(data), total=len(data), desc="Enumerating"):
+            time.sleep(0.2)
+
 
 
 if __name__ == '__main__':
