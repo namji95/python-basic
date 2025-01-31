1dc32ec555940c5069e2ed7cd74dea23ad37c3a2
namji <skawlgns07@naver.com>
2025-01-31 23:46:15 +0900

    feat(selenium): 웹 크롤링 라이브러리 selenium 예제
    웹 크롤링 라이브러리 selenium 사용한 예제 코드
    크롬 동행복권 접속 후 추첨 번호 추출 기능 구현

--- crawler/selenium_test.py
+++ crawler/selenium_test.py
@@ -1,26 +1,40 @@
-# selenium의 webdriver를 사용하기 위한 import
 from selenium import webdriver
-
-# selenium으로 키를 조작하기 위한 import
-# from selenium.webdriver.common.keys import Keys
-
-# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class SeleniumTest:
     def start_selenium(self):
-        # 크롬드라이버 실행
-        driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("")
+        options.add_argument("--disable-blink-features=AutomationControlled")  # 자동화 탐지 방지
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
 
-        # 크롬 드라이버에 url 주소 넣고 실행
-        driver.get('https://www.google.co.kr/')
+        driver = webdriver.Chrome(options=options)
 
-        # 페이지가 완전히 로딩되도록 3초동안 기다림
+        # DevTools로 탐지 방지
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+        driver.get('https://dhlottery.co.kr/gameResult.do?method=byWin&wiselog=C_A_1_2')
+
         time.sleep(3)
+
+        win_numbers = driver.find_element(By.CLASS_NAME, "num.win")
+
+        print("win_numbers.text: ", win_numbers.text)
+
+        time.sleep(3)
+
+        bonus_number = driver.find_element(By.CLASS_NAME, "num.bonus")
+
+        print("bonus_number.text: ", bonus_number.text)
+
+        driver.quit()
 
 if __name__ == '__main__':
     selenium_test = SeleniumTest()
     selenium_test.start_selenium()
-
-
-# todo: 파일 이름 또는 클래스 이름이 라이브러리 이름과 같을 경우 에러 발생 예약어 조심
