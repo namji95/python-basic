fad2c880c04d33d86b9b3957fb429fdc9b5def1d
namji <skawlgns07@naver.com>
2025-02-03 17:20:10 +0900

    refactor(ftp): ftp 파일 경로 수정
    ftp 파일 경로 수정 후 특정 파일 지정하여 다운로드 할 수 있도록 수정

--- data_processing/ftp.py
+++ data_processing/ftp.py
@@ -8,27 +8,32 @@ host = ftp_constant.FTP_HOST
 user = ftp_constant.FTP_USER
 pw = ftp_constant.FTP_PW
 local_dir = ftp_constant.LOCAL_DIRECTORY
-# 파일 리스트 먼저 확인 후 원하는 파일 선택
-local_file_path = os.path.join(local_dir, "파일 선택")
+
+# 다운로드할 파일명 "" 안에 다운로드 하고 싶은 파일 확장자 포함해서 작성
+FILE_NAME = "다운로드 파일"
+local_file_path = os.path.join(local_dir, FILE_NAME)
 
 with ftplib.FTP() as ftp:
-    # ftp 접근
+    # FTP 접속
     ftp.connect(host=host, port=21)
-    # 인코딩
-    ftp.encoding = 'utf-8'
-    # ftp 로그인
-    ftp.login(user=user, passwd=pw)
+    ftp.encoding = 'utf-8'  # 인코딩 설정
+    ftp.login(user=user, passwd=pw)  # 로그인
 
-    # ftp 서버에 존재하는 폴더 및 파일 리스트
-    print(ftp.nlst())
+    # 해당 디렉터리로 이동 "" 안에 디렉터리 경로 작성
+    ftp.cwd("디렉터리 경로")
 
-    # 해당 경로에 해당 폴더가 없으면 생성하는 로직
+    # FTP 서버에 존재하는 파일 리스트 확인
+    files = ftp.nlst()
+    print("FTP 파일 목록:", files)
+
+    # 저장할 로컬 폴더가 없으면 생성
     if not os.path.exists(local_dir):
         os.makedirs(local_dir)
 
-    # 파일 다운로드
-    with open(local_file_path, "wb") as f:
-        # 파일 리스트 읽어 리스트에 있는 파일 선택해서 내려받기
-        ftp.retrbinary("파일 선택", f.write)
-
-    print(f"파일 다운로드 완료: ReadMe.txt")
+    # 파일이 존재하면 다운로드
+    if FILE_NAME in files:
+        with open(local_file_path, "wb") as f:
+            ftp.retrbinary(f"RETR {FILE_NAME}", f.write)
+        print(f"파일 다운로드 완료: {local_file_path}")
+    else:
+        print(f"파일이 존재하지 않습니다: {FILE_NAME}")
