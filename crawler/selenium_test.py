1c3c95539b488d26dbc682504bae34a6fdac047e
namji <skawlgns07@naver.com>
2025-02-04 10:52:20 +0900

    refactor(selenium): 필요 없는 import 제거
    코드 리포맷 작업 (필요 없는 import 제거)

--- crawler/selenium_test.py
+++ crawler/selenium_test.py
@@ -1,10 +1,11 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class SeleniumTest:
     def start_selenium(self):
